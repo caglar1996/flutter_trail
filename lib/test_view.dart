172a0ffaff4e2c8aab5daa3212f080157f53516e
caglar1996 <caglar.karabacak@outlook.com>
2021-03-01 23:55:05 +0300

    First update

---
+++ lib/test_view.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class TesdtView extends StatelessWidget {
+  const TesdtView({Key key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Text("Test View"),
+    );
+  }
+}
